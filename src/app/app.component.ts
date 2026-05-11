887202625bc210b5867de0d636cc2c3d86dffc06
Pavilion Sahota <pavilionsahota@gmail.com>
2026-05-11 21:45:10 +0100

    scss over css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { invoke } from "@tauri-apps/api/core";
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   greetingMessage = "";
