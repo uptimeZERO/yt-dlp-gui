c958b76541f4eeb0fbbd9751369aeccdb8601822
Pavilion Sahota <pavilionsahota@gmail.com>
2026-05-11 19:49:49 +0100

    Updating to angular 21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-
-import { RouterOutlet } from '@angular/router';
 import { invoke } from "@tauri-apps/api/core";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
